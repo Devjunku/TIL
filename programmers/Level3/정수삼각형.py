3d0c1581a46ccc69b1a5f66bf744dbe4fb922be7
박준규 <junku8782@gmail.com>
2021-02-23 20:11:25 +0900

    TIL 2021.02.23_4

--- programmers/Level3/정수삼각형.py
+++ programmers/Level3/정수삼각형.py
@@ -1,6 +1,15 @@
 def solution(triangle):
+    for i in range(1, len(triangle)):
+        for j in range(i+1):
+            if j == 0:
+                triangle[i][j] += triangle[i-1][j]
+            elif i == j:
+                triangle[i][j] += triangle[i-1][j-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
+    return max(triangle[-1])
+    
 
-    return answer
 
 
 if __name__ == '__main__':
