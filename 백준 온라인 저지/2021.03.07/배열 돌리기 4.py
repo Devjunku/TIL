46af3f52b9ed5cb3ec1390acca972fecf148c9b9
박준규 <junku8782@gmail.com>
2021-03-07 19:43:14 +0900

    TIL 2021.03.07_3

--- 백준 온라인 저지/2021.03.07/배열 돌리기 4.py
+++ 백준 온라인 저지/2021.03.07/배열 돌리기 4.py
@@ -3,5 +3,19 @@ N, M, K = map(int, input().split())
 arr = [ list(map(int, input().split())) for _ in range(N)]
 rcs = [ list(map(int, input().split())) for _ in range(K)]
 
+print(arr)
+print(rcs)
+
+
+
+
+right = [1, 0]
+left = [1, 0]
+
+
+
 def rcsfunc(arr, rcs):
+    rcs[][0]-rcs[][2]
+    rcs[][1]-rcs[][2]
+
     pass
